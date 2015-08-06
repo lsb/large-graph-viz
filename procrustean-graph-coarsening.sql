4e98fd55e8ee72f4633d4de7780468760545f542
lsb <leebutterman@gmail.com>
2015-08-06 07:41:43 -0700

    explicit vertices loading, plus no permanent degrees storage

--- procrustean-graph-coarsening.sql
+++ procrustean-graph-coarsening.sql
@@ -1,37 +1,42 @@
 -- ∃ M, ratio of edges to merge
 
 create table raw_edges (v1 text, v2 text);
-create table vertices (id integer primary key, name text);
-create table vcollapse (oldv integer, newv integer, level smallint); -- primary key (oldv, level); clustering idx newv;
+create table vertices (id integer primary key autoincrement, name text);
+create table vcollapse (oldv integer, newv integer, level smallint); -- pk (oldv, level); clustering idx newv;
 create table edges (v1 integer, v2 integer, level smallint, primary key (level, v1, v2));
-create table degrees(id integer, degree integer, level smallint, primary key (level, id));
+
+---
+
+insert into vertices (name)
+  select name from (select v1 as name from raw_edges union all select v2 as name from raw_edges)
+              group by name order by count(*) desc;
+
+insert into edges
+  select case when v1.id < v2.id then v1.id else v2.id end,
+         case when v1.id < v2.id then v2.id else v1.id end,
+         0
+         from raw_edges e join vertices v1 on e.v1 = v1.name join vertices v2 on e.v2 = v2.name group by e.v1, e.v2;
 
 256.times {|LEVEL|
 
-insert into degrees
-  select id, count(*), LEVEL from
-    (select v1 as id from edges where level = LEVEL union all select v2 as id from edges where level = LEVEL)
-    group by id;
-
-create temporary table vcollapse1 (oldv integer primary key, newv integer); create index vc1_n on vcollapse1 (newv);
-
-insert into vcollapse1
-  select e.v2, e.v1 from
-    edges e
-    join degrees d1 on e.level = d1.level and d1.id = e.v1
-    join degrees d2 on e.level = d2.level and d2.id = e.v2
-    where e.level = LEVEL
-    order by d1.degree * 1.0 / d2.degree -- * (U(0,1) + N) -- if non-determinism is desired
-    limit ceil(select count(*)/ <<<M>>> from edges where level = LEVEL) - 1;
-
-cluster vcollapse1 on vc1_n;
-
-with recursive collapsepaths as
-  (select oldv, newv from vcollapse1
-    union all
-    select older.oldv, newer.newv from vcollapse1 older join collapsepaths newer on newer.oldv = older.newv)
+with recursive
+  degrees (id, degree) as
+    (select id, count(*)
+      from (select v1 as id from edges where level = LEVEL union all select v2 as id from edges where level = LEVEL)
+      group by id)
+  vcollapse1 (oldv, newv) as
+    (select e.v2, e.v1
+       from edges e join degrees d1 on d1.id = e.v1 join degrees d2 on d2.id = e.v2
+       where e.level = LEVEL
+       order by d1.degree * 1.0 / d2.degree -- * (U(0,1) + N) -- if non-determinism is desired
+       limit (select count(*)/ <<<M>>> from edges where level = LEVEL))
+  collapsepaths (oldv, newv) as
+    (select oldv, newv from vcollapse1
+      union all
+      select older.oldv, newer.newv from vcollapse1 older join collapsepaths newer on newer.oldv = older.newv)
 insert into vcollapse
   select oldv, min(newv), LEVEL group by oldv;
+
 insert into edges
   select coalesce(v1.newv, e.v1) newv1, coalesce(v2.newv, e.v2) newv2, LEVEL + 1 from
     edges e
@@ -39,7 +44,5 @@ insert into edges
     left join vcollapse v2 on e.level = v2.level and v2.oldv = e.v2
     where e.level = LEVEL and newv1 != newv2
     group by newv1, newv2;
-
-drop table vcollapse1;
 
 }
