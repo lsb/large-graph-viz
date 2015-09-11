b88bf546dfd5c45fe075f93e162f9405684a66e0
lsb <leebutterman@gmail.com>
2015-09-11 18:41:42 -0400

    fewer levels, stricter tolerances

--- clojure-harness/test/clojure_harness/core_test.clj
+++ clojure-harness/test/clojure_harness/core_test.clj
@@ -6,9 +6,9 @@
 (yesql.core/defqueries "sql/test.sql")
 
 (deftest e2e-test
-  (testing "Test whether an end-to-end fdl of an 8-node ring comes out looking like a ring"
+  (testing "Test whether an end-to-end fdl of an 32-node ring comes out looking like a ring"
     (let [c (db-cnxn ":memory:")
-          levels 6
+          levels 5
           node-count (reduce * (replicate levels 2))]
       (init-db! c)
       (create-raw-edges! c)
@@ -16,5 +16,5 @@
       (import-from-raw! c)
       (collapse-levels! c 2 0)
       (randomize-nodes! c levels)
-      (update-positions! c 0.2 1 999 levels 0.02 0.001 0.9)
-      (is (> 3.0 (:stdev (first (angle-stdev c 0 node-count))))))))
+      (update-positions! c 0.2 1 500 levels 0.01 0.01 0.9)
+      (is (> 6.0 (:stdev (first (angle-stdev c 0 node-count))))))))
